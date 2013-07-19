4395e2aa3e96ac99496e3f8003ca288a76fd20c6
Daniel Newton <djpnewton@gmail.com>
2013-07-19 12:02:59 +1200

    tidied usb some. read hid out reports from endpoint callback (now reading reports works on stm32f4)

--- protocol/usb.c
+++ protocol/usb.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <libopencm3/usb/usbd.h>
 #include <libopencm3/usb/hid.h>
-#include <libopencm3/cm3/systick.h>
 #include "prot.h"
 
-#define HID_OUT_PACKET 64
-#define HID_IN_PACKET 64
+#define HID_OUT_REPORTSIZE 64
+#define HID_OUT_EP 0x02
+#define HID_IN_REPORTSIZE 64
+#define HID_IN_EP 0x81
 
 const struct usb_device_descriptor dev = {
 	.bLength = USB_DT_DEVICE_SIZE,
@@ -25,19 +26,19 @@ const struct usb_device_descriptor dev = {
 };
 
 static const uint8_t hid_report_descriptor[] = {
-	0x06, 0xFF, 0x00,       // usage page
-	0x0A, 0x01, 0x00,       // usage
-	0xA1, 0x01,             // Collection 0x01
-	0x75, 0x08,             // report size = 8 bits
-	0x15, 0x00,             // logical minimum = 0
-	0x26, 0xFF, 0x00,       // logical maximum = 255
-	0x95, HID_OUT_PACKET,   // report count
-	0x09, 0x01,             // usage
-	0x81, 0x02,             // Input (array)
-	0x95, HID_IN_PACKET,    // report count
-	0x09, 0x02,             // usage
-	0x91, 0x02,             // Output (array)
-	0xC0                    // end collection 
+	0x06, 0xFF, 0x00,        // usage page
+	0x0A, 0x01, 0x00,        // usage
+	0xA1, 0x01,              // Collection 0x01
+	0x75, 0x08,              // report size = 8 bits
+	0x15, 0x00,              // logical minimum = 0
+	0x26, 0xFF, 0x00,        // logical maximum = 255
+	0x95, HID_OUT_REPORTSIZE,// report count
+	0x09, 0x01,              // usage
+	0x81, 0x02,              // Input (array)
+	0x95, HID_IN_REPORTSIZE, // report count
+	0x09, 0x02,              // usage
+	0x91, 0x02,              // Output (array)
+	0xC0                     // end collection 
 };
 
 static const struct {
@@ -64,17 +65,17 @@ const struct usb_endpoint_descriptor hid_endpoint[2] = {
 	{
 		.bLength = USB_DT_ENDPOINT_SIZE,
 		.bDescriptorType = USB_DT_ENDPOINT,
-		.bEndpointAddress = 0x81,
+		.bEndpointAddress = HID_IN_EP,
 		.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
-		.wMaxPacketSize = HID_IN_PACKET,
+		.wMaxPacketSize = HID_IN_REPORTSIZE,
 		.bInterval = 0x20,
 	},
 	{
 		.bLength = USB_DT_ENDPOINT_SIZE,
 		.bDescriptorType = USB_DT_ENDPOINT,
-		.bEndpointAddress = 0x02,
+		.bEndpointAddress = HID_OUT_EP,
 		.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
-		.wMaxPacketSize = HID_OUT_PACKET,
+		.wMaxPacketSize = HID_OUT_REPORTSIZE,
 		.bInterval = 0x20,
 	}
 };
@@ -129,16 +130,26 @@ static int hid_control_request(usbd_device *usbd_dev, struct usb_setup_data *req
 	(void)complete;
 	(void)usbd_dev;
 
-	if ((req->bmRequestType != 0x81) ||
+	if ((req->bmRequestType != (USB_REQ_TYPE_IN | USB_REQ_TYPE_INTERFACE)) ||
 	   (req->bRequest != USB_REQ_GET_DESCRIPTOR) ||
-	   (req->wValue != 0x2200))
-		return 0;
+	   (req->wValue != USB_DT_REPORT << 8))
+		return USBD_REQ_NOTSUPP;
 
 	/* Handle the HID report descriptor. */
 	*buf = (uint8_t *)hid_report_descriptor;
 	*len = sizeof(hid_report_descriptor);
 
-	return 1;
+	return USBD_REQ_HANDLED;
+}
+
+static void hid_rx(usbd_device *usbd_dev, uint8_t ep)
+{
+	uint8_t buf[HID_OUT_REPORTSIZE] = {};
+
+	if (usbd_ep_read_packet(usbd_dev, HID_OUT_EP, buf, HID_OUT_REPORTSIZE) > 0)
+	{
+		prot_process_packet(buf, HID_OUT_REPORTSIZE);
+	}	
 }
 
 static void hid_set_config(usbd_device *usbd_dev, uint16_t wValue)
@@ -146,32 +157,26 @@ static void hid_set_config(usbd_device *usbd_dev, uint16_t wValue)
 	(void)wValue;
 	(void)usbd_dev;
 
-	usbd_ep_setup(usbd_dev, 0x81, USB_ENDPOINT_ATTR_INTERRUPT, HID_IN_PACKET, NULL);
-	usbd_ep_setup(usbd_dev, 0x02, USB_ENDPOINT_ATTR_INTERRUPT, HID_OUT_PACKET, NULL);
+	usbd_ep_setup(usbd_dev, HID_IN_EP, USB_ENDPOINT_ATTR_INTERRUPT, HID_IN_REPORTSIZE, NULL);
+	usbd_ep_setup(usbd_dev, HID_OUT_EP, USB_ENDPOINT_ATTR_INTERRUPT, HID_OUT_REPORTSIZE, hid_rx);
 
 	usbd_register_control_callback(
 				usbd_dev,
 				USB_REQ_TYPE_STANDARD | USB_REQ_TYPE_INTERFACE,
 				USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
 				hid_control_request);
-
-	systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
-	// SysTick interrupt every N clock pulses: set reload to N-1
-	systick_set_reload(99999);
-	systick_interrupt_enable();
-	systick_counter_enable();
 }
 
 static usbd_device *usbd_dev;
 
 static void prot_send_buffer(void* buffer, size_t buffer_size)
 {
-	uint8_t buf[HID_IN_PACKET] = {};
-	int size = HID_IN_PACKET;
+	uint8_t buf[HID_IN_REPORTSIZE] = {};
+	int size = HID_IN_REPORTSIZE;
 	if (buffer_size < size)
 		size = buffer_size;
 	memcpy(buf, buffer, size);
-	while (usbd_ep_write_packet(usbd_dev, 0x81, buf, HID_IN_PACKET) == 0);
+	while (usbd_ep_write_packet(usbd_dev, HID_IN_EP, buf, HID_IN_REPORTSIZE) == 0);
 }
 
 void usb_init(void)
@@ -179,12 +184,13 @@ void usb_init(void)
 	int i;
 
 #ifdef STM32F1
-	usbd_dev = usbd_init(&stm32f103_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
+	const usbd_driver* driver = &stm32f103_usb_driver;
 #elif STM32F4
-	usbd_dev = usbd_init(&otgfs_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
+	const usbd_driver* driver = &otgfs_usb_driver;
 #else
 ERROR
 #endif
+	usbd_dev = usbd_init(driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
 	usbd_register_set_config_callback(usbd_dev, hid_set_config);
 
 	for (i = 0; i < 0x80000; i++)
@@ -197,15 +203,5 @@ void usb_poll_forever(void)
 {
 	while (1)
 		usbd_poll(usbd_dev);
-}
-
-void sys_tick_handler(void)
-{
-	uint8_t buf[HID_OUT_PACKET] = {};
-
-	if (usbd_ep_read_packet(usbd_dev, 0x02, buf, HID_OUT_PACKET) > 0)
-	{
-		prot_process_packet(buf, HID_OUT_PACKET);
-	}
 }
 
