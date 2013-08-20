14ad9a1fe116f23183c53df43303ce041d021619
Daniel Newton <djpnewton@gmail.com>
2013-08-20 17:18:17 +1200

    missed some stuff on pipe1 (device discovery channel)

--- protocol/wireless.c
+++ protocol/wireless.c
@@ -40,13 +40,25 @@ static void _nrf_init(int mode, uint8_t addr[AG_ADDR_LEN])
     // setup discovery channel for all slaves
     if (mode == AG_MODE_SLAVE)
     {
-        // enable pipe1
+        // enable pipe1 RX address
         nrf_read_reg(NRF_REG_EN_RXADDR, &buf);
-        nrf_set_reg_field(NRF_REG_EN_RXADDR, NRF_REGF_ERX_P1,  &buf, 1);
+        nrf_set_reg_field(NRF_REG_EN_RXADDR, NRF_REGF_ERX_P1, &buf, 1);
         nrf_write_reg(NRF_REG_EN_RXADDR, &buf);
+        // enable pipe1 auto ACK
+        nrf_read_reg(NRF_REG_EN_AA, &buf);
+        nrf_set_reg_field(NRF_REG_EN_AA, NRF_REGF_ENAA_P1, &buf, 1);
+        nrf_write_reg(NRF_REG_EN_AA, &buf);
         // RX_ADDR_P1 - set receive address data pipe1
         memcpy(buf.data, _discovery_address, AG_ADDR_LEN);
         nrf_write_reg(NRF_REG_RX_ADDR_P1, &buf);
+        // Enable dynamic payload width on pipe1
+        nrf_read_reg(NRF_REG_DYNPD, &buf);
+        nrf_set_reg_field(NRF_REG_DYNPD, NRF_REGF_DPL_P1, &buf, 1);
+        nrf_write_reg(NRF_REG_DYNPD, &buf);
+        // RX_PW_P1 - set number of bytes in RX payload in data PIPE1
+        nrf_read_reg(NRF_REG_RX_PW_P1, &buf);
+        nrf_set_reg_field(NRF_REG_RX_PW_P1, NRF_REGF_PW, &buf, sizeof(struct ag_payload_t));
+        nrf_write_reg(NRF_REG_RX_PW_P1, &buf);
     }
 
     // wait for radio to power up
